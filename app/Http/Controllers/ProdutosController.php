e4982d5746b3c5c4aed3601cc2b29c7f3f541936
jdbilia <jdbilia@gmail.com>
2022-07-20 10:31:07 -0300

    versão 1

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto as ModelsProduto;
+use Illuminate\Http\Request;
+use App\Models\Produto;
+use Illuminate\Support\Facades\Http;
+
+
+class ProdutosController extends Controller
+{
+    public function create()
+    {
+        return view('produtos.create');
+    }
+
+    public function store(Request $request)
+    {
+        Produto::create([
+            'nome' => $request->nome,
+            'custo' => $request->custo,
+            'preco' => $request->preco,
+            'quantidade' => $request->quantidade,
+        ]);
+
+        return "Produto Criado com Sucesso!";
+    }
+
+    public function show($id)
+    {
+        $produto = Produto::findOrFail($id);
+        return view('produtos.show', ['produto' => $produto]);
+    }
+
+    public function edit($id)
+    {
+        $produto = Produto::findOrFail($id);
+        return view('produtos.edit', ['produto' => $produto]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $produto = Produto::findOrFail($id);
+
+        $produto->update([
+            'nome' => $request->nome,
+            'custo' => $request->custo,
+            'preco' => $request->preco,
+            'quantidade' => $request->quantidade,
+        ]);
+
+        return "Produto Atualizado com Sucesso!";
+    }
+
+    public function delete($id)
+    {
+        $produto = Produto::findOrFail($id);
+        return view('produtos.delete', ['produto' => $produto]);
+    }
+
+    public function destroy($id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->delete();
+
+        return "Produto Excluído com Sucesso";
+    }
+}
